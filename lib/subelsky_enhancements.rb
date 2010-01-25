55d1db3b782ba5dc96b817e72b0f3da177b7f08f
Mike Subelsky <github@mikeshop.net>
2010-01-25 17:40:55 -0500

    Slight documentation note - mainly done as a test of rdoc

--- lib/subelsky_enhancements.rb
+++ lib/subelsky_enhancements.rb
@@ -1,5 +1,8 @@
 cwd = File.dirname(__FILE__)
 
+# Requiring this file loads up all the enhancements, which can also be cherry-picked 
+# by requiring them individually.
+
 %w(hash).each do |lib|
   require File.join(cwd,"subelsky_enhancements",lib)
 end
